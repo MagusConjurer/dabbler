cdfa7d548da85f5e9f9660d3ac96cafcc4c61ea8
Joshua Duncan <jduncan97@gmail.com>
2020-04-13 14:54:35 -0600

    adding more jss and html

---
+++ assets/javascript/app.js
@@ -0,0 +1,26 @@
+$("#searchBox").hide();
+
+$("#movieButton").click(function () {getMovies()});
+$("#musicButton").click(function () {getMusic()});
+$("#booksButton").click(function () {getBooks()});
+$("#gamesButton").click(function () {getGames()});
+
+function getMovies() {
+    //show searchBox on click
+    $("#searchBox").show();
+}
+
+function getMusic() {
+    //show searchBox on click
+    $("#searchBox").show();
+}
+
+function getBooks() {
+    //show searchBox on click
+    $("#searchBox").show();
+}
+
+function getGames() {
+    //show searchBox on click
+    $("#searchBox").show();
+}
