6438ec527932fb31b16edec090e6b148307520b7
Joshua Duncan <jduncan97@gmail.com>
2020-04-13 15:45:05 -0600

    added search bar changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,26 +1,42 @@
-$("#searchBox").hide();
+var movieSearch = ('<div class="input-group">' +
+'<input type="text" class="form-control" placeholder="Movies" id="movieInfo">' + 
+'<button class="btn btn-outline-dark" type="button" id="searchbutton">Search</button>' +
+'</div>');
+var musicSearch = ('<div class="input-group">' +
+'<input type="text" class="form-control" placeholder="Music" id="musicInfo">' + 
+'<button class="btn btn-outline-dark" type="button" id="searchbutton">Search</button>' +
+'</div>');
+var booksSearch = ('<div class="input-group">' +
+'<input type="text" class="form-control" placeholder="Books" id="booksInfo">' + 
+'<button class="btn btn-outline-dark" type="button" id="searchbutton">Search</button>' +
+'</div>');
+var gamesSearch = ('<div class="input-group">' +
+'<input type="text" class="form-control" placeholder="Games" id="gamesInfo">' + 
+'<button class="btn btn-outline-dark" type="button" id="searchbutton">Search</button>' +
+'</div>');
 
 $("#movieButton").click(function () {getMovies()});
 $("#musicButton").click(function () {getMusic()});
 $("#booksButton").click(function () {getBooks()});
 $("#gamesButton").click(function () {getGames()});
 
-function getMovies() {
-    //show searchBox on click
-    $("#searchBox").show();
+function getMovies(){
+    //need to clear search box and results with each button click
+    $("#searchBox").empty()
+    $("#searchBox").append(movieSearch);
 }
 
-function getMusic() {
-    //show searchBox on click
-    $("#searchBox").show();
+function getMusic(){
+    $("#searchBox").empty()
+    $("#searchBox").append(musicSearch);
 }
 
-function getBooks() {
-    //show searchBox on click
-    $("#searchBox").show();
+function getBooks(){
+    $("#searchBox").empty()
+    $("#searchBox").append(booksSearch);
 }
 
-function getGames() {
-    //show searchBox on click
-    $("#searchBox").show();
+function getGames(){
+    $("#searchBox").empty()
+    $("#searchBox").append(gamesSearch);
 }
